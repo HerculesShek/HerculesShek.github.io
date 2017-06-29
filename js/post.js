ba795fd9a9506c4f1eea67786a55f5a6a9eef8fa
HerculesShek <hercules.shek@gmail.com>
2017-06-29 14:24:14 +0800

    update disqus

--- js/post.js
+++ js/post.js
@@ -1,38 +1,46 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-    var isMobile = {
-        Android: function() {
-            return navigator.userAgent.match(/Android/i);
-        }
-        ,BlackBerry: function() {
-            return navigator.userAgent.match(/BlackBerry/i);
-        }
-        ,iOS: function() {
-            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-        }
-        ,Opera: function() {
-            return navigator.userAgent.match(/Opera Mini/i);
-        }
-        ,Windows: function() {
-            return navigator.userAgent.match(/IEMobile/i);
-        }
-        ,any: function() {
-            return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-        }
-    };
+            var isMobile = {
+                Android: function() {
+                    return navigator.userAgent.match(/Android/i);
+                },
+                BlackBerry: function() {
+                    return navigator.userAgent.match(/BlackBerry/i);
+                },
+                iOS: function() {
+                    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+                },
+                Opera: function() {
+                    return navigator.userAgent.match(/Opera Mini/i);
+                },
+                Windows: function() {
+                    return navigator.userAgent.match(/IEMobile/i);
+                },
+                any: function() {
+                    return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+                }
+            };
 
-    $('pre').addClass('prettyprint linenums'); //添加Google code Hight需要的class
+            $('pre').addClass('prettyprint linenums'); //添加Google code Hight需要的class
 
-    //***********************
-    //**评论的代码也删掉哦***
-    window.disqus_shortname = 'herculesshek'; // required: replace example with your forum shortname
-    $('#disqus_container .comment').on('click',function(){
-        $(this).html('加载中...');
-        var that = this;
-        $.getScript('http://' + disqus_shortname + '.disqus.com/embed.js',function(){$(that).remove()});
-    });
-    //**评论的代码也删掉哦***
-    //***********************
+            //***********************
+            //**评论的代码也删掉哦***
+            //window.disqus_shortname = 'herculesshek'; // required: replace example with your forum shortname
+            $('#disqus_container .comment').on('click', function() {
+                $(this).html('加载中...');
+                // var that = this;
+                // $.getScript('http://' + disqus_shortname + '.disqus.com/embed.js',function(){$(that).remove()});
+                var that = this;
+                var d = document,
+                    s = d.createElement('script');
+                s.src = 'https://will-shek.disqus.com/embed.js';
+                s.setAttribute('data-timestamp', +new Date());
+                (d.head || d.body).appendChild(s);
+
+                $(that).remove(); // function() { $(that).remove() };
+            });
+            //**评论的代码也删掉哦***
+            //***********************
 
     $('.entry a').each(function(index,element){
         var href = $(this).attr('href');
